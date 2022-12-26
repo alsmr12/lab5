f3d1f07e62a344a4401609dccb4158c57ee48f73
alsmr12 <aleksejsmirnov183@gmail.com>
2022-12-26 17:08:33 +0300

    .

--- pr1/main.c
+++ pr1/main.c
@@ -10,15 +10,15 @@
 int main(int argc, char *argv[]) {
     FILE *f1;
     FILE *f2;
-    getopt(argc, argv, "o:i:a::f::d::");
+    getopt(argc, argv, "i:o:a::f::d::");
     f1 = fopen(optarg, "r");
-    getopt(argc, argv, "o:i:a::f::d::");
+    getopt(argc, argv, "i:o:a::f::d::");
     f2 = fopen(optarg, "w");
     int numsort = 0;
-    char *field;
-    char *dir;
+    char *field = NULL;
+    char *dir = NULL;
     int opt;
-    while ((opt = getopt(argc, argv, "o:i:a::f::d::")) != -1) {
+    while ((opt = getopt(argc, argv, "i:o:a::f::d::")) != -1) {
 		switch (opt) {
 		    case 'a':
                 numsort = (int)*optarg;
@@ -33,13 +33,16 @@ int main(int argc, char *argv[]) {
                 printf("\n! Была проигнорированна неверно введённая опция\n");
         }
     }
-    int (*cmp)(const el*, const el*);
-    if (strcmp(field, "age") == 0 && strcmp(dir, "inc") == 0) cmp = el_cmp_age_increasing;
-    if (strcmp(field, "fio") == 0 && strcmp(dir, "inc") == 0) cmp = el_cmp_fio_increasing;
-    if (strcmp(field, "ps") == 0 && strcmp(dir, "inc") == 0) cmp = el_cmp_ps_increasing;
-    if (strcmp(field, "age") == 0 && strcmp(dir, "dec") == 0) cmp = el_cmp_age_decreasing;
-    if (strcmp(field, "fio") == 0 && strcmp(dir, "dec") == 0) cmp = el_cmp_fio_decreasing;
-    if (strcmp(field, "ps") == 0 && strcmp(dir, "dec") == 0) cmp = el_cmp_ps_decreasing;
+
+    int (*cmp)(const el*, const el*) = el_cmp_fio_increasing;
+    if (field != NULL && dir != NULL) {
+        if (strcmp(field, "age") == 0 && strcmp(dir, "inc") == 0) cmp = el_cmp_age_increasing;
+        if (strcmp(field, "fio") == 0 && strcmp(dir, "inc") == 0) cmp = el_cmp_fio_increasing;
+        if (strcmp(field, "ps") == 0 && strcmp(dir, "inc") == 0) cmp = el_cmp_ps_increasing;
+        if (strcmp(field, "age") == 0 && strcmp(dir, "dec") == 0) cmp = el_cmp_age_decreasing;
+        if (strcmp(field, "fio") == 0 && strcmp(dir, "dec") == 0) cmp = el_cmp_fio_decreasing;
+        if (strcmp(field, "ps") == 0 && strcmp(dir, "dec") == 0) cmp = el_cmp_ps_decreasing;
+    }
 
     el *arr = NULL;
     int len;
@@ -47,16 +50,19 @@ int main(int argc, char *argv[]) {
     clock_t begin, end;
     switch(numsort) {
         case '3':
+            printf("\nСортировка вставками\n");
             begin = clock();
             insert_sort(arr, len, cmp);
             end = clock();
             break;
         case '2':
+            printf("\nСортировка расчёской\n");
             begin = clock();
             comb_sort(arr, len, cmp);
             end = clock();
             break;
         case '1':
+            printf("\nБиблиотечный qsort\n");
             begin = clock();
             qsort(arr, len, sizeof(el),
                 (int (*)(const void *, const void *)) cmp);
@@ -66,7 +72,7 @@ int main(int argc, char *argv[]) {
             printf("\nАлгоритм сортировки не обозначен.\nБиблиотечный qsort использован по умолчанию\n");
             begin = clock();
             qsort(arr, len, sizeof(el),
-                 (int (*)(const void *, const void *)) el_cmp_fio_decreasing);
+                 (int (*)(const void *, const void *)) cmp);
             end = clock();
             break;
     }
