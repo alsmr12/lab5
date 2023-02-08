dd5671db299dcfcb87ede833b312595dd8040a81
alsmr12 <aleksejsmirnov183@gmail.com>
2023-02-08 16:48:04 +0300

    .

--- pr2/struct.c
+++ pr2/struct.c
@@ -4,29 +4,6 @@
 
 #include "struct.h"
 
-/*char *freadline(FILE *f) {
-    char buf[81];
-    char *str = NULL;
-    int len = 0, t;
-    do {
-        t = fscanf(f, "%80[^\n]", buf);
-        if (t == -1) {
-            return NULL;
-        }
-        else if (t == 0) fscanf(f, "%*c");
-        else {
-            int len_buf = strlen(buf);
-            str = (char *)realloc(str, (len + len_buf + 1) * sizeof(char));
-            memcpy(str + len, buf, len + len_buf);
-            len = len + len_buf;
-        }
-    } while ((t > 0));
-    if (len == 0) str = (char *)malloc(1 * sizeof(char));
-    str[len] = '\0';
-    return str;
-}*/
-
-
 void el_print(const el *e, FILE *f) {
     fprintf(f, "%s, %s, %d\n", e -> fio, e -> ps, e -> age);
 }
